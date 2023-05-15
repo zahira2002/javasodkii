e2de6172b20493737a92bf9c16b91b39325131db
zahira2002 <129403417+zahira2002@users.noreply.github.com>
2023-05-15 21:48:10 +0100

    Update ConsoleClass.java
    with dbb

--- console/ConsoleClass.java
+++ console/ConsoleClass.java
@@ -1,18 +1,13 @@
 package com.console;
 
-import java.util.Random;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.game.classes.Echiquier;
-import com.game.classes.Lion;
-import com.game.classes.Loup;
 import com.game.classes.Piece;
 import com.game.tools.MoveException;
 import com.game.tools.Point;
 
+import java.util.Scanner;
 public class ConsoleClass {
+
 
     private static int direction;
 
@@ -39,7 +34,6 @@ public class ConsoleClass {
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
 
-
             String[] trait = command.split(",");
 
             // echiquier.switchPlayer();
@@ -57,7 +51,11 @@ public class ConsoleClass {
                 } else if ("l".equals(trait[1])) {
                     direction = 4;
                 }
+
+
+
                 try {
+
                     choosenPiece.movePiece(direction, 0);
                 } catch (MoveException | NullPointerException e) {
                     System.out.println("Cant Move");
@@ -72,54 +70,78 @@ public class ConsoleClass {
     }
 
     public static void HumanContreMachine(Echiquier echiquier) {
-
         while (true) {
-            if(echiquier.getTurn()==-1) {
-                System.out.println("saisir votre Commande player"+echiquier.getTurn());
+            if (echiquier.getTurn() == -1) {
+                System.out.println("Saisir votre commande player " + echiquier.getTurn());
                 Scanner scanner = new Scanner(System.in);
                 String command = scanner.nextLine();
 
                 String[] trait = command.split(",");
 
-                // echiquier.switchPlayer();
                 Piece choosenPiece = echiquier.getPieceByLabel(trait[0]);
                 if (choosenPiece != null && trait.length == 2) {
-                    System.out.println("you choose : " + choosenPiece);
-                    System.out.println("to make this move " + trait[1]);
-
-                    if ("a".equals(trait[1])) {
+                    String move = trait[1];
+                    int direction = 0;
+                    if ("a".equals(move)) {
                         direction = 1;
-                    } else if ("b".equals(trait[1])) {
+                    } else if ("b".equals(move)) {
                         direction = 2;
-                    } else if ("r".equals(trait[1])) {
+                    } else if ("r".equals(move)) {
                         direction = 3;
-                    } else if ("l".equals(trait[1])) {
+                    } else if ("l".equals(move)) {
                         direction = 4;
                     }
 
-
-
                     try {
-
+                        Point depart = choosenPiece.getPosition();
                         choosenPiece.movePiece(direction, 0);
+                        Point arrivee = choosenPiece.getPosition();
+
+                        // Enregistrer le déplacement dans la base de données
+                        echiquier.enregistrerDeplacement(choosenPiece, depart, arrivee);
                     } catch (MoveException | NullPointerException e) {
-                        System.out.println("Cant Move");
+                        System.out.println("Impossible de déplacer la pièce.");
                     }
-
                 } else {
-                    System.out.println("Cant Find Piece Or command not correct");
+                    System.out.println("Impossible de trouver la pièce ou commande incorrecte.");
                 }
+            } else {
+                // La machine joue
+                Piece[] pieces = echiquier.getPieceByTurn().toArray(new Piece[0]);
+                Piece choosenPiece = pieces[(int) (Math.random() * pieces.length)];
+                int direction = (int) (Math.random() * 4) + 1;
 
+                try {
+                    Point depart = choosenPiece.getPosition();
+                    choosenPiece.movePiece(direction, 0);
+                    Point arrivee = choosenPiece.getPosition();
 
-            }
-            else {
-                echiquier.randomTurn();
+                    // Enregistrer le déplacement dans la base de données
+                    echiquier.enregistrerDeplacement(choosenPiece, depart, arrivee);
+                } catch (MoveException | NullPointerException e) {
+                    // Si le déplacement est impossible, on en choisit un autre aléatoirement
+                    while (true) {
+                        choosenPiece = pieces[(int) (Math.random() * pieces.length)];
+                        direction = (int) (Math.random() * 4) + 1;
+                        try {
+                            Point depart = choosenPiece.getPosition();
+                            choosenPiece.movePiece(direction, 0);
+                            Point arrivee = choosenPiece.getPosition();
 
+                            // Enregistrer le déplacement dans la base de données
+                            echiquier.enregistrerDeplacement(choosenPiece, depart, arrivee);
+
+                            break;
+                        } catch (MoveException | NullPointerException ex) {
+                            // Si le déplacement est encore impossible, on continue la boucle
+                        }
+                    }
+                }
             }
             echiquier.showEchiquier();
         }
-
     }
+
 
     public static void main(String[] args) {
 
@@ -163,7 +185,6 @@ public class ConsoleClass {
             default:
                 break;
         }
-
 
     }
 }
